34759f46978b8d1e3c071b792c8a6674d02f573f
CadeBrown <brown.cade@gmail.com>
2018-02-16 18:43:40 -0500

    new version, subcommands

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='vpl',
-    version='0.0.1',
+    version='0.0.2',
 
     description='Video Pipe Line',
     long_description=long_description,
